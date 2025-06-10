6dd4b8367e2ffa9922407b58fa76ad96e808f96e
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-06-10 13:08:10 -0400

    Implemented testing function for MCMC sampling routine.

---
+++ tests/testGridDensitySampling.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('../')
+
+import numpy as np
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+
+from Density1DOptimizers import reflected_hmc_from_tabulated_density
+
+def testGridMCMCSampling():
+    # Setup the distribution
+    S = lambda x: np.tanh(x)
+    D = 0.1
+    L = 10
+    mu = lambda x: np.exp( (S(x) + S(x)**3 / 6.0) / D)
+    grid = np.linspace(-L, L, 101)
+    mu_grid = mu(grid)
+    Z = np.trapz(mu_grid, grid)
+    mu_grid /= Z
+
+    # Sampling parameters
+    N = 10**5
+    step = 1.0
+
+    # Do sampling
+    samples = reflected_hmc_from_tabulated_density(grid, mu_grid, N, step)
+
+    # Plot samples versus density
+    x_array = np.linspace(-L, L, 1001)
+    dist = np.exp( (S(x_array) + S(x_array)**3 / 6.0) / D)
+    Z = np.trapz(dist, x_array)
+    dist = dist / Z
+    plt.hist(samples, density=True, bins=100, color='tab:orange', label='MCMC')
+    plt.plot(x_array, dist, linestyle='--', linewidth=2, color='tab:red', label='Steady State')
+    plt.xlabel('x')
+    plt.ylabel(r'$\mu(x)$')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    testGridMCMCSampling()
