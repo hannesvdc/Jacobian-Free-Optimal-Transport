5f2d8c97877b6d4255a9830662bf653bcaf12e75
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-07-28 20:52:50 +0200

    Removing original FV timestepper.

--- EconomicPDETimestepper.py
+++ EconomicPDETimestepper.py
@@ -1,56 +1,6 @@
 import numpy as np
 import numpy.linalg as lg
 
-def step(rho, x_faces, dt, gamma, vplus, vminus, eplus, eminus, g):
-    N = len(x_faces) - 1 # We have N midpoints
-    assert N % 2 == 1 # Ensure x = 0 is a midpoint!
-    h = 2.0 / N
-
-    # Calculate the temporal model parameters
-    drho_dx_right = (0.0 - rho[-1]) / h
-    drho_dx_left = (rho[0] - 0.0) / h
-    Q_matrix = np.array([[1.0 + 0.5 * gamma * eplus**2 * drho_dx_right, 0.5 * gamma * eminus**2 * drho_dx_right], \
-                         [-0.5*gamma*eplus**2 * drho_dx_left, 1.0 - 0.5 * gamma * eminus**2 * drho_dx_left]])
-    velocities = lg.solve(Q_matrix, np.array([vplus, vminus]))
-    sigmasq = velocities[0] * eplus**2 + velocities[1] * eminus**2
-    Rplus = -0.5 * sigmasq * drho_dx_right
-    Rminus = 0.5 * sigmasq * drho_dx_left
-    S = Rplus + Rminus
-
-    # Calculate the values and gradient of the local density at the cell boundaries
-    c = velocities[0] * eplus + velocities[1] * eminus
-    mu = gamma * x_faces - c
-    rho_upwind = np.where(mu[1:-1] > 0, rho[:-1],  rho[1:])
-    rho_grad = (rho[1:] - rho[:-1]) / h
-
-    # Compute the fluxes at the interior cell boundaries, and its gradient in the cell centers
-    J_inner = -mu[1:-1] * rho_upwind - 0.5 * sigmasq * rho_grad
-
-    # left boundary  (face 0, x = -1)
-    rho_up_L  = 0.0 if mu[0] > 0 else rho[0]
-    J_left    = (-mu[0] * rho_up_L - 0.5 * sigmasq * (rho[0] - 0.0) / h)
-
-    # right boundary (face N, x = 1)
-    rho_up_R  = 0.0 if mu[-1] < 0 else rho[-1]
-    J_right   = (-mu[-1] * rho_up_R - 0.5 * sigmasq * (0.0 - rho[-1]) / h)
-
-    # Update rho everywhere
-    J = np.concatenate(([J_left], J_inner, [J_right]))
-    rho_new = rho - dt * (J[1:] - J[:-1]) / h
-
-    # Approximate the delta function by a Gaussian
-    eps = 0.01
-    x_centres = 0.5 * (x_faces[1:] + x_faces[:-1])
-    gaussian = np.exp(-x_centres**2 / (2.0 * eps**2)) / np.sqrt(2.0 * np.pi * eps**2)
-    rho_new += dt * S * gaussian
-    #print(np.any(np.isnan(rho_new)))
-
-    # Ensure the new density is normalized
-    area = np.sum(rho_new * h)
-    rho_new = rho_new / area
-
-    return rho_new
-
 def FV_step(rho, x_faces, dt, gamma, vpc, vmc, eplus, eminus, g):
     N = len(x_faces) - 1 # We have N midpoints
     assert N % 2 == 1 # Ensure x = 0 is a midpoint!
