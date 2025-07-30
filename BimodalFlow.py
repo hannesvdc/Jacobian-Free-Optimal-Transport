25c20bfd6b9bc5503ea3f809da6290987b069d09
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-07-30 09:51:26 +0200

    We can obtain the correct velocity field from running Euler OT timestepper

---
+++ BimodalFlow.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+U = lambda x: 0.5 * (x**2 - 1.0)**2
+dU = lambda x: 2.0 * (x**2 - 1.0) * x
+
+def particleTimestepper(X, h, beta, rng):
+    return X - h * dU(X) + np.sqrt(2.0 / beta) * rng.normal(0.0, 1.0, len(X))
+
+# Assumes X is sorted!
+def velocityField(X, h, beta, rng):
+    Y = particleTimestepper(X, h, beta, rng)
+    Y = np.sort(Y)
+
+    return (Y - X) / h
+
+# Assumes X is sorted!
+def averagedVelocityField(X, hmax, hmin, beta, rng):
+    h_values = np.logspace(np.log10(hmin), np.log10(hmax), num=10, base=10.0)
+    velocity_field = np.zeros_like(X)
+    for h in h_values:
+        v_h = velocityField(X, h, beta, rng)
+        velocity_field += v_h
+
+    return velocity_field / len(h_values)
+
+def estimateBimodalPotential():
+    xmin = -3.0
+    xmax = 3.0
+
+    # Construct the basis functions at collocation points
+    sigma = 0.5
+    basis_functions = []#[lambda x: x**2, lambda x: x]
+    nabla_basis_functions = []#[lambda x: 2*x, lambda x: 1.0 * np.ones_like(x)]
+    collocation_points = np.linspace(xmin, xmax, 101)
+    for p in collocation_points:
+        rbf = lambda x, loc=p: np.exp(-(x - loc)**2 / (2.0 * sigma**2))
+        nabla_rbf = lambda x, loc=p: -(x - loc) / sigma**2 * np.exp(-(x - loc)**2 / (2.0 * sigma**2))
+        basis_functions.append(rbf)
+        nabla_basis_functions.append(nabla_rbf)
+    psi = lambda x: np.array([bf(x) for bf in basis_functions])
+    dpsi = lambda x: np.array([dbf(x) for dbf in nabla_basis_functions])
+
+    # Generate a uniform particle distribution
+    N = 100001
+    X0 = np.linspace(xmin, xmax, N) # sorted
+    dx = 10.0 / (N-1)
+    hmin = 1.e-2
+    hmax = 1.e-0
+    beta = 2.0
+    print('X0', X0)
+
+    # Approximate the velocity field in X
+    rng = np.random.RandomState()
+    v = averagedVelocityField(X0, hmax, hmin, beta, rng)
+
+    # try one x-value
+    x_try = -1.5
+    print(psi(x_try), dpsi(x_try))
+
+    # Estimate the coefficients through least-squares analysis
+    nabla_psi_vals = dpsi(X0)
+    G = nabla_psi_vals @ nabla_psi_vals.T / N
+    b = -(nabla_psi_vals @ v[:,np.newaxis] / N)[:,0]
+    theta = np.linalg.solve(G, b)
+    print('Theta values', theta)
+    
+    # Build the potential energy approximation and exponentiate it.
+    U_parametric = lambda x: -np.dot(theta, psi(x))
+    dU_parametric = lambda x: -np.dot(theta, dpsi(x))
+    U_values = U_parametric(X0)
+    U_values -= np.min(U_values)
+
+    # Compare the analytic with estimated density
+    plt.plot(X0, v, label='Estimated Velocity Field')
+    plt.plot(X0, dU_parametric(X0), label='Analytic Velocity Field')
+    plt.xlabel(r'$x$')
+    plt.legend()
+
+    plt.figure()
+    plt.plot(X0, U(X0), label='Exact Potential')
+    plt.plot(X0, U_values, label='Estimated Potential')
+    plt.xlabel(r'$x$')
+    plt.legend()
+
+    plt.figure()
+    mu_exact = np.exp(-beta * U(X0))
+    mu_exact /= np.trapz(mu_exact * dx, X0)
+    mu_estimated = np.exp(-beta * U_values)
+    mu_estimated /= np.trapz(mu_estimated * dx, X0)
+    plt.plot(X0, mu_exact, label='Exact Density')
+    plt.plot(X0, mu_estimated, label='Estimated Density')
+    plt.xlabel(r'$x$')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    estimateBimodalPotential()
